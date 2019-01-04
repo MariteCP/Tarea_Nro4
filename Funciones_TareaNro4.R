fb213735500a2d9f1f521148eabac248fe321ec6
MariteCP <44910194+MariteCP@users.noreply.github.com>
2019-01-04 18:57:54 -0300

    Funciones_TareaNro4

---
+++ Funciones_TareaNro4.R
@@ -0,0 +1,15 @@
+#dv: int -> int
+#Def dv: funcion para generar digito verificador en un rut valido
+#Ejemplo: dv(18862668)->8
+
+dv <- function(rut){
+  rut = as.character(rut)
+  x = as.numeric(rev(strsplit(rut,NULL)[[1]]))
+  Multiplo = rep(2:7,length.out=length(x))
+  y = sum(x*Multiplo)
+  z = 11 - y + floor(y/11)*11
+  key = c(1:11)
+  val = c(1:9,"k",0)
+  dv = val[match(z, key)]
+  return(dv)
+}
